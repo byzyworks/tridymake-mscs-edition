3194cc37f02c80528f63e9c38213d0300f81e603
John Ellis <johnellis173@gmail.com>
2022-03-16 21:43:22 -0400

    Added deep-copier function.

--- habitat/src/utility/common.js
+++ habitat/src/utility/common.js
@@ -5,12 +5,26 @@ export const app = {
 
 export const overlay = (target, source) => {
     for (const property in source) {
-        if (typeof source[property] == 'object') {
+        if ((typeof source[property] === 'object') && (source[property] !== null)) {
             overlay(target[property], source[property]);
         } else {
             target[property] = source[property];
         }
     }
+}
+
+export const deepCopy = (source) => {
+    const target = Array.isArray(source) ? [ ] : { };
+
+    for (const property in source) {
+        if ((typeof source[property] === 'object') && (source[property] !== null)) {
+            target[property] = deepCopy(source[property]);
+        } else {
+            target[property] = source[property];
+        }
+    }
+    
+    return target;
 }
 
 export const isEmpty = (obj) => {
